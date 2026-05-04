97bf98d8e060273558a35f897b5bd262d02b2e0c
William Leonheart <supercoolwizard08@gmail.com>
2026-05-04 21:18:52 +0300

    made function generator instead of hard-coding paths, might be a problem with metadata_maker due to assumption of order

--- src/utils/data_manager.py
+++ src/utils/data_manager.py
@@ -6,45 +6,74 @@ import pandas as pd
 
 
 class DataManager:
-    def __init__(self, file_extension, data_path, input_path):
-        self.file_extension = file_extension
+    def __init__(self, audio_file_extension, transcripts_file_extension, data_path, input_path):
+        self.audio_file_extension = audio_file_extension
+        self.transcripts_files_extension = transcripts_file_extension
         self.data_path = data_path
         self.input_path = input_path
-        self.train_dir = self.data_path / "train"
-        self.test_dir = self.data_path / "test"
-        self.train_audio_dir = self.train_dir / "audio"
-        self.test_audio_dir = self.test_dir / "audio"
-        self.train_transcripts_dir = self.train_dir / "transcripts"
-        self.test_transcripts_dir = self.test_dir / "transcripts"
-        self.train_transcripts_dir = self.train_dir / "transcripts"
-        self.test_transcripts_dir = self.test_dir / "transcripts"
+        self.dirs = {
+            "train": self.data_path / "train",
+            "test": self.data_path / "test"
+        }
+
+
+    def get_path(self, split, category, role=None):
+        """e.g. self.get_path('train','transcripts','teacher')"""
+        path = self.dirs[split] / category
+        if role:
+            path = path / role
+
+        path.mkdir(parents=True, exist_ok=True)
+        return path
+
 
     def get_total_input_duration(self):
         pass
 
+
     def train_test_splitter(self):
         """"performs dummy split, copies smaller part to test/audio and larger part to train/audio"""
-        input_files = [f for f in self.input_path.iterdir() if f.suffix == self.file_extension]
+        input_files = [f for f in self.input_path.iterdir() if f.suffix == self.audio_file_extension]
         train_count = int(np.floor(len(input_files) * 0.8))
 
-        train_files = input_files[:train_count]
-        test_files = input_files[train_count:]
+        files_split = {
+            "train": input_files[:train_count],
+            "test": input_files[train_count:]
+        }
 
-        for file in train_files:
-            shutil.copy2(file, self.train_audio_dir)
+        for split, files in files_split.items():
+            dest = self.get_path(split, "audio")
+            for file in files:
+                shutil.copy2(file, dest)
 
-        for file in test_files:
-            shutil.copy2(file, self.test_audio_dir)
 
     def metadata_maker(self):
         """creates metadata.csv that connects transcripts with audio files"""
-        train_files_audio = [f for f in self.train_audio_dir.iterdir() if f.suffix == self.file_extension]
-        train_files_audio = [f for f in self.train_audio_dir.iterdir() if f.suffix == self.file_extension]
-        train_files_audio = [f for f in self.train_audio_dir.iterdir() if f.suffix == self.file_extension]
-        metadata_df = pd.DataFrame({"audio_path": train_files_audio, "student_path": train_files_audio, "teacher_path": train_files_audio, })
+        for split in ["train", "test"]:
+            current_split_dir = self.dirs[split]
 
-        metadata_df.to_csv(self.train_audio_dir / "metadata.csv", index=False)
+            audio_dir = self.get_path(split, "audio")
+            audio_files = [f for f in audio_dir.iterdir() if f.suffix == self.audio_file_extension]
 
-    def transcripts_filler(self, text):
-       pass 
+            student_dir = self.get_path(split, "transcripts", "student")
+            student_files = [f for f in student_dir.iterdir() if f.suffix == self.transcripts_files_extension]
 
+            teacher_dir = self.get_path(split, "transcripts", "teacher")
+            teacher_files = [f for f in teacher_dir.iterdir() if f.suffix == self.transcripts_files_extension]
+
+            metadata_df = pd.DataFrame({"audio_path": audio_files, "student_path": student_files, "teacher_path": teacher_files})
+            metadata_df.to_csv(current_split_dir / "metadata.csv", index=False)
+
+
+    def transcripts_files_creator(self):
+        """creates empty transcript fiiles, for them to be then filled"""
+        for split in ["train", "test"]: 
+            reference_path = self.get_path(split, "audio")
+            reference_files = [f for f in reference_path.iterdir() if f.suffix == self.audio_file_extension]
+            reference_names = [f.stem for f in reference_files]
+
+            for role in ["student", "teacher"]:
+                current_path = self.get_path(split, "transcripts", role)
+
+                for name in reference_names:
+                    open(current_path / f"{name}{self.transcripts_files_extension}", "x")
--- tests/unit/test_data_manager.py
+++ tests/unit/test_data_manager.py
@@ -1,8 +1,8 @@
 from src.utils.data_manager import *
 from src.config import settings
 
-dm = DataManager(".mp3", settings.DATA_DIR, settings.INPUT_DIR)
+dm = DataManager(".mp3", ".txt", settings.DATA_DIR, settings.INPUT_DIR)
 # print(dm.train_test_splitter())
-dm.train_test_splitter()
+# dm.train_test_splitter()
 
-
+dm.metadata_maker()
