7299ef124f3cc2b5bbf1909aef1a19f0a2ec2507
William Leonheart <supercoolwizard08@gmail.com>
2026-05-16 19:29:10 +0300

    hotfix: student_trainer.py required arguments, that were never passed

--- scripts/student_trainer.py
+++ scripts/student_trainer.py
@@ -1,16 +1,9 @@
 from src.fine_tuning.student_trainer import StudentTrainer
 from src.adapters.huggingface import get_hf_token, get_repo_name
 from src.config import settings
-import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="train student_model"
-    )
-
-    parser.add_argument()
-
     token = get_hf_token()
     repo = get_repo_name()
 
